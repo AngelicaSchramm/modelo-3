c3c739a5891aa13efb1d8187b8ce3c4c7381a13d
AngelicaSchramm <angelicaschramm77@gmail.com>
2022-09-18 23:38:17 -0300

    efeito-3

--- script.js
+++ script.js
@@ -1,21 +1,11 @@
-document.addEventListener("DOMContentLoaded", ()=>{
-    let content = document.querySelectorAll('.efeito');
-    content.forEach((el) =>{
-        let contentPosition = el.getBoundingClientRect().top;
-        let screenPosition = window.innerHeight;
-        if(contentPosition < screenPosition/1.2){
-            el.classList.add("animate");
-        }
-    });
-});
-
 document.addEventListener("scroll", ()=>{
     let content = document.querySelectorAll('.efeito');
     content.forEach((el) =>{
         let contentPosition = el.getBoundingClientRect().top;
         let screenPosition = window.innerHeight;
-        if(contentPosition < screenPosition/1.2){
+        if(contentPosition < screenPosition/1.1){
             el.classList.add("animate");
         }
     });
 });
+
