0300a82f4be9ad8f6f0676fd863201c693064110
AngelicaSchramm <angelicaschramm77@gmail.com>
2022-09-19 13:23:36 -0300

    efeito-4

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+document.addEventListener("DOMContentLoaded", ()=> {
+    let el = document.querySelector('.efeito-1');
+    el.classList.add("animate");
+})
+
+
 document.addEventListener("scroll", ()=>{
-    let content = document.querySelectorAll('.efeito');
+    let content = document.querySelectorAll('.efeito-2');
     content.forEach((el) =>{
         let contentPosition = el.getBoundingClientRect().top;
         let screenPosition = window.innerHeight;
